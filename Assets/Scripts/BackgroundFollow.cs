d91e9730c87ef9eb709e1e15f77fe11b82a37887
John Ricaurte <jjricaurtem@gmail.com>
2022-01-30 14:50:09 -0500

    Versión 0.8 - Fondos aleatorios

--- Assets/Scripts/BackgroundFollow.cs
+++ Assets/Scripts/BackgroundFollow.cs
@@ -1,19 +1,20 @@
-using System;
 using UnityEngine;
 
 public class BackgroundFollow : MonoBehaviour
 {
+    [SerializeField] private Sprite[] backgroundList;
+    private SpriteRenderer _spriteRenderer;
     private float _startParentPositionX;
 
-    private Camera _mainCamera;
     private float _startPositionX;
 
     // Start is called before the first frame update
     private void Start()
     {
-        _mainCamera = Camera.main;
         _startParentPositionX = transform.parent.position.x;
         _startPositionX = transform.localPosition.x;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = GetRandomBackground();
     }
 
     // Update is called once per frame
@@ -22,6 +23,11 @@ public class BackgroundFollow : MonoBehaviour
         var pos = transform.parent.position.x - _startParentPositionX;
         var offset = (int)(pos / 5);
         Debug.Log(offset);
-        transform.localPosition = new Vector2(_startPositionX-(offset/100f), transform.localPosition.y);
+        transform.localPosition = new Vector2(_startPositionX - offset / 100f, transform.localPosition.y);
+    }
+
+    private Sprite GetRandomBackground()
+    {
+        return backgroundList[Random.Range(0, backgroundList.Length)];
     }
 }
