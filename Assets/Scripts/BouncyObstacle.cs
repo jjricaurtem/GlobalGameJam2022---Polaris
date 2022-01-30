cbf82108afbb62d821fa8f36a72987eae0633acb
John Ricaurte <jjricaurtem@gmail.com>
2022-01-29 22:02:30 -0500

    Versión 1.5 - rebote arreglado

--- Assets/Scripts/BouncyObstacle.cs
+++ Assets/Scripts/BouncyObstacle.cs
@@ -1,27 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BouncyObstacle : MonoBehaviour
 {
-    [SerializeField]private float yDifferenceRank;
+    [SerializeField] private float yDifferenceRank;
     [SerializeField] private float bouncyAmount;
+
     public void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.tag.Equals("Frontal") && IsInFrontOf(collider))
+        if (collider.tag.Equals("Frontal"))
         {
-            Debug.Log("Hits in the front "+transform.name+" with "+collider.name);
-            // var temp = PlayerController.sharedInstance.transform;
-            // temp.position = new Vector3(temp.position.x - bouncyAmount, temp.position.y);
+            Debug.Log("Hits in the front " + transform.name + "-" + transform.parent.name + " with " + collider.name);
+            var temp = PlayerController.sharedInstance.transform;
+            temp.position = new Vector3(temp.position.x - bouncyAmount, temp.position.y);
             PlayerController.sharedInstance.bounce = true;
         }
-    }
-
-    public bool IsInFrontOf(Collider2D collider)
-    {
-        var yDiff = transform.position.y - collider.transform.position.y;
-        yDiff = Math.Abs(yDiff);
-        return yDiff < yDifferenceRank;
     }
 }
--- Assets/Scripts/LeaveBlockTrigger.cs
+++ Assets/Scripts/LeaveBlockTrigger.cs
@@ -2,10 +2,12 @@
 
 public class LeaveBlockTrigger : MonoBehaviour
 {
+    private bool _alreadyTriggered = false;
     public void OnTriggerEnter2D(Collider2D collider)
     {
-        if (!collider.tag.Equals("Player")) return;
+        if (!collider.tag.Equals("Player")|| _alreadyTriggered) return;
         LevelGenerator.sharedInstance.AddNewBlock();
         LevelGenerator.sharedInstance.RemoveOldBlock();
+        _alreadyTriggered = true;
     }
 }
